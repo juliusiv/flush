c54d5048fec69c632bd6db7d29795eec593f3b36
Julius Alexander <julius@plangrid.com>
2019-02-24 18:50:13 -0800

    Make requests to Lifx.

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,89 @@
-extern crate hyper;
 extern crate sysfs_gpio;
 
-use hyper::{self, Method, Request, Uri, Write};
-use hyper::header::CONTENT_TYPE;
-use hyper::rt::{self, Future, Stream};
-use hyper::Client;
+use reqwest;
+use reqwest::Client;
 use sysfs_gpio::{Direction, Edge, Pin};
 use std::env;
-use std::io::prelude::*;
-use std::io::stdout;
+// use std::io::prelude::*;
+// use std::io::stdout;
 
-fn interrupt(pin: u64) -> sysfs_gpio::Result<()> {
-    let input = Pin::new(pin);
-    input.with_exported(|| {
-        input.set_direction(Direction::In)?;
-        input.set_edge(Edge::BothEdges)?;
-        let mut poller = input.get_poller()?;
-        loop {
-            match poller.poll(1000)? {
-                Some(value) => println!("{}", value),
-                None => {
-                    let mut stdout = stdout();
-                    stdout.write_all(b".")?;
-                    stdout.flush()?;
-                }
-            }
-        }
-    })
-}
+// fn interrupt(pin: u64) -> sysfs_gpio::Result<()> {
+//     let input = Pin::new(pin);
+//     input.with_exported(|| {
+//         input.set_direction(Direction::In)?;
+//         input.set_edge(Edge::BothEdges)?;
+//         let mut poller = input.get_poller()?;
+//         loop {
+//             match poller.poll(1000)? {
+//                 Some(value) => println!("{}", value),
+//                 None => {
+//                     let mut stdout = stdout();
+//                     stdout.write_all(b".")?;
+//                     stdout.flush()?;
+//                 }
+//             }
+//         }
+//     })
+// }
 
-fn main() {
-    let client = Client::new();
-    let uri: Uri = .parse().unwrap();
-    let token = "OAUTH_TOKEN";
+fn make_pulse_request(client: &Client, token: &str) -> Result<(), reqwest::Error> {
     let json = r##"{
         "color":"#f47442",
         "period":0.5,
         "cycles":4,
         "power_on":true
     }"##;
-    // let mut req = Request::new(Body::from(json));
-    let req = Request::builder()
-        .method("POST")
-        .uri("https://api.lifx.com/v1/lights/:selector/effects/pulse")
+    client
+        .post("https://api.lifx.com/v1/lights/all/effects/pulse")
         .header("Authorization", format!("Bearer {}", token))
         .header("Content-Type", "application/json")
-        .body(Body::from(json))
-        .unwrap();
-    // *req.method_mut() = Method::POST;
-    // *req.uri_mut() = uri.clone();
-    // req.headers_mut().insert(
-    //     CONTENT_TYPE,
-    //     HeaderValue::from_static("application/json")
-    // );
+        .body(json)
+        .send()?;
 
-    client
-        .request(req)
-        .and_then(|resp| {
-            println!("POST: {}", resp.status());
-            resp.into_body().concat2()
-        })
+    return Ok(());
+}
 
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        println!("Usage: ./interrupt <pin>");
-    } else {
-        match args[1].parse::<u64>() {
-            Ok(pin) => {
-                match interrupt(pin) {
-                    Ok(()) => println!("Interrupting Complete!"),
-                    Err(err) => println!("Error: {}", err),
+fn run_event_loop(token: &str) {
+    let client = Client::new();
+
+    // The switch should be connected to pin ___.
+    let input = Pin::new(3);
+    let result = input.with_exported(|| {
+        input.set_direction(Direction::In)?;
+        input.set_edge(Edge::BothEdges)?;
+        let mut poller = input.get_poller()?;
+
+        println!("Beginning event loop");
+
+        loop {
+            match poller.poll(1000)? {
+                Some(value) => println!("{}", value),
+                None => {
+                    match make_pulse_request(&client, token) {
+                        Ok(()) => println!("Interrupting Complete!"),
+                        Err(_) => println!("Error!"),
+                    }
                 }
             }
-            Err(_) => println!("Usage: ./interrupt <pin>"),
         }
+    });
+
+    match result {
+        Ok(_) => println!("Nothing went wrong, nice!"),
+        Err(err) => println!("{:?}", err)
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: ./flush <token>");
+    } else {
+        let token = &args[1];
+
+        // let client = Client::new();
+        // make_pulse_request(&client, token).unwrap();
+
+        run_event_loop(token);
     }
 }
